c7c52f0f17897c7a0dd78dff9b64626c26766202
Sam <pashag,fanpage@gmail.com>
2022-12-12 15:43:12 +0330

    did some backend work in user posts

--- post/models.py
+++ post/models.py
@@ -8,4 +8,7 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=1000)
     created_date = models.DateField()
-    
+    url = models.SlugField(max_length=500, unique=True, blank=True, editable=False)
+    def save(self, *args, **kwargs):
+        self.url = slugify(self.title)
+        super(Post, self).save(*args, **kwargs)
--- post/views.py
+++ post/views.py
@@ -1,10 +1,19 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from .forms import AddPostForm
 from .models import Post as Task
 
 def addPost(request):
+    form = AddPostForm(request.POST)
     if request.method == 'POST':
-        form = AddPostForm(request.POST)
         Post = Task.objects.all()
-    return render(request, 'post/addPost.html')
+        if form.is_valid():
+            form.save()
+        return HttpResponseRedirect("/")
+    context = {
+        'form': form,
+    }
+    return render(request, 'post/addPost.html', context)
+
+
