9f360158a5480c1a910448e506fdb7cfa552bd15
Sam <pashag,fanpage@gmail.com>
2022-12-12 15:22:04 +0330

    Almost added the posting function

--- post/forms.py
+++ post/forms.py
@@ -2,6 +2,9 @@ from django import forms
 from .models import Post
 
 class AddPostForm(forms.Form):
+    title = forms.CharField(max_length=100)
+    description = forms.CharField(max_length=1000)
+    Due_To = forms.DateField()
     class Meta:
         model = Post
         fields = ['title', 'description', 'created_date']
--- post/views.py
+++ post/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import AddPostForm
+from .models import Post as Task
 
 def addPost(request):
-    form = AddPostForm()
-    return render(request, 'post/addPost.html', {'form': form})
-
-    
-
+    if request.method == 'POST':
+        form = AddPostForm(request.POST)
+        Post = Task.objects.all()
+    return render(request, 'post/addPost.html')
